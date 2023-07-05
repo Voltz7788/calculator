af194623af2bd1c80de61eff52d06bf0228e75a3
Yannis <yannismadu777@gmail.com>
2023-07-05 15:14:28 +0100

    Add functions to set up buttons on UI

--- logic.js
+++ logic.js
@@ -1,4 +1,13 @@
+let num1 = "";
+let num2 = "";
+let operator = "";
+
+let num1Available = true;
+let num2Available = true;
+
 function operate(operator, num1, num2) {
+    num1 = Number(num1);
+    num2 = Number(num2);
     if (operator === "+") {
         return num1 + num2;
     } else if (operator === "-") {
@@ -10,11 +19,49 @@ function operate(operator, num1, num2) {
     };
 };
 
+function setUpNumberButtons() {
+    let numberButtons = document.querySelectorAll(".numButton");
+    numberButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            if (num1Available) {
+                num1 += button.id;
+            } else if (num2Available) {
+                num2 += button.id;
+            };
+        });
+    });
+};
 
-function startApp() {
-    let num1;
-    let num2;
-    let operator;
+function setUpOperatorButtons() {
+    let operatorButtons = document.querySelectorAll(".opButton");
+    operatorButtons.forEach(button => {
+        button.addEventListener("click", () => {
 
+            operator = button.id;
+
+            if (num1Available) {
+                num1Available = false;
+                console.log(num1);
+            } else if (num2Available) {
+                console.log(num2);
+                num2Available = false;
+                console.log(operate(operator, num1, num2));
+            }
+        });
+    });
+};
+
+function setUpClearButton() {
 
 };
+
+
+
+
+function startApp() {
+    setUpNumberButtons()
+    setUpOperatorButtons()
+    setUpClearButton()
+};
+
+startApp()
